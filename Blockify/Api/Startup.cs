0d49df6db2f0c1ce6818e1400e2342522056f260
DanielPadre58 <2kupwhite@gmail.com>
2025-08-15 18:48:30 +0100

    Swagger configuration implementation

--- Blockify/Api/Startup.cs
+++ Blockify/Api/Startup.cs
@@ -2,7 +2,9 @@ using Blockify.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+SwaggerConfiguration.ConfigureSwagger(builder.Services);
 
 DependencyInjector.Inject(builder.Services, builder.Configuration);
 
@@ -10,9 +12,11 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    SwaggerConfiguration.ConfigureSwaggerUI(app);
 }
 
-app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
