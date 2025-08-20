fb77764a2061195aac65ef6e35e195311f0adba2
DanielPadre58 <2kupwhite@gmail.com>
2025-08-20 20:43:38 +0100

    Exception for handling missing claims

---
+++ Blockify/Shared/Exceptions/MissingPrincipalClaimException.cs
@@ -0,0 +1,8 @@
+namespace Blockify.Shared.Exceptions {
+    public class MissingPrincipalClaimException : Exception
+    {
+        public MissingPrincipalClaimException() : base("There is a missing claim in the authentication result provided") { }
+        
+        public MissingPrincipalClaimException(string claimName) : base($"{claimName} is missing claim in the authentication result provided") { }
+    }
+}
