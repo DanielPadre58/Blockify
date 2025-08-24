95bcd957727bb76a86d4177529458163d4d541d8
DanielPadre58 <2kupwhite@gmail.com>
2025-08-24 16:18:50 +0100

    Added inner exception to all exceptions

--- Blockify/Shared/Exceptions/AuthenticationException.cs
+++ Blockify/Shared/Exceptions/AuthenticationException.cs
@@ -1,5 +1,11 @@
-namespace Blockify.Shared.Exceptions {
-    public class AuthenticationException : Exception {
-        public AuthenticationException() : base("Unable to authenticate user"){}
+namespace Blockify.Shared.Exceptions
+{
+    public class AuthenticationException : Exception
+    {
+        public AuthenticationException()
+            : base("Unable to authenticate user") { }
+
+        public AuthenticationException(string message, Exception? innerException)
+            : base(message, innerException) { }
     }
 }
