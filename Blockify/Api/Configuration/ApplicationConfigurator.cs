402c2817d332e66d66cab9d55d157cfcc48ec93c
DanielPadre58 <2kupwhite@gmail.com>
2025-08-25 18:07:54 +0100

    Added custom custom claims to spotify oauth options add scopes to the spotify api

--- Blockify/Api/Configuration/ApplicationConfigurator.cs
+++ Blockify/Api/Configuration/ApplicationConfigurator.cs
@@ -1,5 +1,8 @@
+using System.Security.Claims;
+using AspNet.Security.OAuth.Spotify;
 using Blockify.Application.Services.Spotify;
 using Blockify.Shared.Exceptions;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Blockify.Api.Configuration
 {
@@ -21,37 +24,64 @@ namespace Blockify.Api.Configuration
             _services.AddOpenApi();
             _services
                 .AddAuthentication("default_cookie")
-                .AddCookie(
-                    "default_cookie",
-                    options =>
-                    {
-                        options.Cookie.Name = "blockifyCookie";
-                        options.ExpireTimeSpan = TimeSpan.FromDays(30);
-
-                        options.LoginPath = "/account/login";
-                        options.LogoutPath = "/account/logout";
-                        options.AccessDeniedPath = "/account/denied";
-                    }
-                )
-                .AddSpotify(
-                    "spotify",
-                    options =>
-                    {
-                        options.ClientId =
-                            _configuration["Api:OAuth:Spotify:ClientId"]
-                            ?? throw new MissingConfigurationException("Spotify Client Id");
-
-                        options.ClientSecret =
-                            _configuration["Api:OAuth:Spotify:ClientSecret"]
-                            ?? throw new MissingConfigurationException("Spotify Client Secret");
-
-                        options.SaveTokens = true;
-                    }
-                );
+                .AddCookie("default_cookie", CookieOptions())
+                .AddSpotify("spotify", SpotifyOptions());
 
             _services.Configure<SpotifyConfiguration>(
                 _configuration.GetSection("Api:OAuth:Spotify")
             );
+        }
+
+        private static Action<CookieAuthenticationOptions> CookieOptions()
+        {
+            return options =>
+            {
+                options.Cookie.Name = "blockifyCookie";
+                options.ExpireTimeSpan = TimeSpan.FromDays(30);
+
+                options.LoginPath = "/account/login";
+                options.LogoutPath = "/account/logout";
+                options.AccessDeniedPath = "/account/denied";
+            };
+        }
+
+        private Action<SpotifyAuthenticationOptions> SpotifyOptions()
+        {
+            return options =>
+            {
+                options.ClientId =
+                    _configuration["Api:OAuth:Spotify:ClientId"]
+                    ?? throw new MissingConfigurationException("Spotify Client Id");
+
+                options.ClientSecret =
+                    _configuration["Api:OAuth:Spotify:ClientSecret"]
+                    ?? throw new MissingConfigurationException("Spotify Client Secret");
+
+                options.SaveTokens = true;
+
+                options.Scope.Add("user-read-private");
+                options.Scope.Add("playlist-modify-public");
+                options.Scope.Add("user-read-email");
+                options.Scope.Add("playlist-read-private");
+
+                options.Events.OnCreatingTicket = async context =>
+                {
+                    context.Identity?.AddClaim(new Claim("urn:provider", "spotify"));
+
+                    context.Identity?.AddClaim(
+                        new Claim("iat", DateTimeOffset.Now.ToUnixTimeSeconds().ToString())
+                    );
+
+                    context.Identity?.AddClaim(
+                        new Claim(
+                            "exp",
+                            DateTimeOffset.Now.AddDays(30).ToUnixTimeSeconds().ToString()
+                        )
+                    );
+
+                    await Task.CompletedTask;
+                };
+            };
         }
 
         public void AddDependencies()
