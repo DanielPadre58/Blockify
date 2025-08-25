d6ed9963493f87f8a12d0e7f68dd85d7e4da921a
DanielPadre58 <2kupwhite@gmail.com>
2025-08-25 20:15:43 +0100

    Adds a new claim to the blockify-cookie, the user's blockify id, and minor refactoring

--- Blockify/Application/Services/Authentication/UserAuthenticationService.cs
+++ Blockify/Application/Services/Authentication/UserAuthenticationService.cs
@@ -1,10 +1,10 @@
 using System.Security.Claims;
-using AspNet.Security.OAuth.Spotify;
 using Blockify.Application.DTOs;
 using Blockify.Application.DTOs.Authentication;
 using Blockify.Domain.Entities;
 using Blockify.Shared.Exceptions;
 using Microsoft.AspNetCore.Authentication;
+using static Blockify.Domain.Entities.User;
 
 namespace Blockify.Application.Services
 {
@@ -12,54 +12,73 @@ namespace Blockify.Application.Services
     {
         public async Task<UserAuthenticationDto> AuthenticateUserAsync(HttpContext context)
         {
-            var result = await context.AuthenticateAsync("spotify");
+            var result =
+                await context.AuthenticateAsync("spotify")
+                ?? throw new Exception("Something went wrong during authentication.");
 
-            if (result?.Succeeded == true)
+            if (!result.Succeeded == true)
+                throw new AuthenticationException("Spotify authentication failed.");
+
+            var authData = new UserAuthenticationDto()
             {
-                var accessToken =
-                    await context.GetTokenAsync("spotify", "access_token")
-                    ?? throw new MissingPrincipalClaimException("Token.AccessToken");
-                var refreshToken =
-                    await context.GetTokenAsync("spotify", "refresh_token")
-                    ?? throw new MissingPrincipalClaimException("Token.RefreshToken");
-                var userId =
-                    result.Principal?.FindFirst(ClaimTypes.NameIdentifier)?.Value
-                    ?? throw new MissingPrincipalClaimException("userId");
-                var userName =
-                    result.Principal?.FindFirst(ClaimTypes.Name)?.Value
-                    ?? throw new MissingPrincipalClaimException("userName");
-
-                await context.SignInAsync("default_cookie", result.Principal!, result.Properties!);
-
-                var user = new User()
+                User = new UserDto
                 {
-                    Spotify = new User.SpotifyData()
+                    Id = Guid.NewGuid(),
+                    Spotify = new SpotifyData()
                     {
-                        Id = userId,
-                        Url = SpotifyAuthenticationDefaults
-                            .UserInformationEndpoint.Replace("api", "open")
-                            .Replace("v1", "user")
-                            .Replace("me", userId),
-                        Username = userName,
-                        RefreshToken = refreshToken,
+                        Id =
+                            result.Principal?.FindFirst(ClaimTypes.NameIdentifier)?.Value
+                            ?? throw new MissingPrincipalClaimException("spotify:userId"),
+                        Username =
+                            result.Principal?.FindFirst(ClaimTypes.Name)?.Value
+                            ?? throw new MissingPrincipalClaimException("spotify:username"),
+                        RefreshToken =
+                            result.Properties?.GetTokenValue("refresh_token")
+                            ?? throw new AuthenticationException("Refresh token not found."),
                     },
-                    CreationDate = DateTime.Now,
-                    LastRequestDate = DateTime.Now,
-                };
+                },
+                Token = new TokenDto
+                {
+                    AccessToken =
+                        result.Properties?.GetTokenValue("access_token")
+                        ?? throw new AuthenticationException("Access token not found."),
+                    TokenType =
+                        result.Properties?.GetTokenValue("token_type")
+                        ?? throw new AuthenticationException("Token type not found."),
+                    Expiry = DateTimeOffset
+                        .Parse(
+                            result.Properties?.GetTokenValue("expires_at")
+                                ?? throw new AuthenticationException("Token expiry not found.")
+                        )
+                        .ToUnixTimeSeconds(),
+                    RefreshToken =
+                        result.Properties?.GetTokenValue("refresh_token")
+                        ?? throw new AuthenticationException("Refresh token not found."),
+                },
+            };
 
-                return new(
-                    new UserDto() { Id = user.Id, Spotify = user.Spotify },
-                    new TokenDto
-                    {
-                        AccessToken = accessToken,
-                        TokenType = "Bearer",
-                        ExpiresIn = 3600,
-                        RefreshToken = refreshToken,
-                    }
-                );
-            }
+            User user = CreateUser(authData);
 
-            throw new AuthenticationException();
+            result
+                .Principal.Identities.First()
+                .AddClaim(new Claim("urn:blockify:spotify_user_id", user.Id.ToString()));
+            await context.SignInAsync("default_cookie", result.Principal!, result.Properties!);
+
+            return authData;
+        }
+
+        private User CreateUser(UserAuthenticationDto authData)
+        {
+            return new User()
+            {
+                Id = authData.User!.Id,
+                Spotify = authData.User.Spotify,
+                CreationDate = DateTime.Now,
+                LastRequestDate = DateTime.Now,
+            };
+
+            //Check if user exists in database
+            //Save user to database
         }
     }
 }
