13017c0dd555d955dc2b30278b1b4bedf7523f64
DanielPadre58 <2kupwhite@gmail.com>
2025-08-29 21:33:07 +0100

    Removed Base adress from spotify client

--- Blockify/Api/Configuration/DependencyInjector.cs
+++ Blockify/Api/Configuration/DependencyInjector.cs
@@ -2,6 +2,7 @@ using Blockify.Application.Services;
 using Blockify.Application.Services.Spotify;
 using Blockify.Application.Services.Spotify.Client;
 using Blockify.Domain.Database;
+using Blockify.Shared.Exceptions;
 using Npgsql;
 
 namespace Blockify.Api.Configuration
@@ -10,18 +11,19 @@ namespace Blockify.Api.Configuration
     {
         public static void Inject(IServiceCollection services, IConfiguration configuration)
         {
+            var test = configuration.GetConnectionString("BlockifyDb");
             services.AddScoped<IBlockifyDbService, BlockifyDbService>(options =>
             {
                 var npgsqlDataSourceBuilder = new NpgsqlDataSourceBuilder(
-                    configuration.GetConnectionString("Blockify")
+                    configuration.GetConnectionString("BlockifyDb")
+                        ?? throw new MissingConfigurationException(
+                            "\"BlockifyDb\" connection string"
+                        )
                 );
                 return new BlockifyDbService(npgsqlDataSourceBuilder);
             });
             services.AddScoped<IUserAuthenticationService, UserAuthenticationService>();
-            services.AddHttpClient<ISpotifyClient, SpotifyClient>(options =>
-            {
-                options.BaseAddress = new Uri("https://accounts.spotify.com/api/");
-            });
+            services.AddHttpClient<ISpotifyClient, SpotifyClient>();
             services.AddScoped<ISpotifyService, SpotifyService>();
         }
     }
