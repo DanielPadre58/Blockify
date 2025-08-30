ad1188f7cffbca5aa4008bd89c1460940e6e06ef
DanielPadre58 <2kupwhite@gmail.com>
2025-08-30 18:33:28 +0100

    previous commit

--- Blockify/Api/Controllers/BlockifyController.cs
+++ Blockify/Api/Controllers/BlockifyController.cs
@@ -14,6 +14,23 @@ namespace Blockify.Application.DTOs.External
             _spotifyService = spotifyService;
         }
 
+        [HttpGet("user/playlists")]
+        public async Task<IActionResult> GetUsersPlaylists() {
+            try
+            {
+                var userId = Convert.ToInt64(User.FindFirst("urn:blockify:user_id")?.Value);
+                var response = await _spotifyService.GetUsersPlaylists(userId);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(
+                    500,
+                    "An error occurred while fetching user playlists: " + ex.Message
+                );
+            }
+        }
+
         [HttpGet("playlist/{playlistId}")]
         public async Task<IActionResult> GetPlaylist([FromRoute] string playlistId)
         {
