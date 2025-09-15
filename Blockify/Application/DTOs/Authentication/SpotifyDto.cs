fca3e0d8f5e6fdbc1acdd11d8d73c1a023675b2c
DanielPadre58 <2kupwhite@gmail.com>
2025-09-15 20:05:07 +0100

    ?

--- Blockify/Application/DTOs/Authentication/SpotifyDto.cs
+++ Blockify/Application/DTOs/Authentication/SpotifyDto.cs
@@ -6,13 +6,13 @@ public record SpotifyDto
 {
     [JsonPropertyName("id")]
     public required string Id { get; set; }
-    
+
     [JsonPropertyName("profile_url")]
     public required string Url { get; set; }
-    
+
     [JsonPropertyName("username")]
     public required string Username { get; set; }
-    
-    [JsonPropertyName("token_data")] 
+
+    [JsonPropertyName("token_data")]
     public required TokenDto Token { get; set; }
 }
