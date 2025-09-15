dbae68babf3f7a5c1ac6ec71c0fbdccee719b427
DanielPadre58 <2kupwhite@gmail.com>
2025-09-15 20:02:59 +0100

    Now the GetAccessTokenById returns a TokenDto and not only the access token, and was renamed to GetAccessToken

--- Blockify/Domain/Database/IBlockifyDbService.cs
+++ Blockify/Domain/Database/IBlockifyDbService.cs
@@ -8,7 +8,7 @@ public interface IBlockifyDbService
     public Task<bool> UsersExistsAsync(string spotifyId);
     public Task<User> InsertUserAsync(User user);
     public Task RefreshAccessTokenAsync(long userId, TokenDto token);
-    public Task<string?> GetAccessTokenByIdAsync(long userId);
+    public Task<TokenDto> GetTokenByIdAsync(long userId);
     public Task<User?> SelectUserByIdAsync(long userId);
     public Task<User?> SelectUserBySpotifyIdAsync(string userId);
 }
