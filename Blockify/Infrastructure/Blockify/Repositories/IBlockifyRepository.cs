c961cc4ea0696d89bdc73e1d18d6793426a13af9
DanielPadre58 <2kupwhite@gmail.com>
2025-09-18 01:26:09 +0100

    feat: a repository to handle database queries, previously known as BlockifyDbbService.cs

---
+++ Blockify/Infrastructure/Blockify/Repositories/IBlockifyRepository.cs
@@ -0,0 +1,16 @@
+using Blockify.Application.DTOs;
+using Blockify.Application.DTOs.Authentication;
+using Blockify.Domain.Entities;
+
+namespace Blockify.Infrastructure.Blockify.Repositories;
+
+public interface IBlockifyDbService
+{
+    public Task<bool> UsersExistsAsync(string spotifyId);
+    public Task<UserDto?> InsertUserAsync(User user);
+    public Task RefreshAccessTokenAsync(long userId, TokenDto token);
+    public Task<TokenDto> GetTokenByIdAsync(long userId);
+    public Task<UserDto?> SelectUserByIdAsync(long userId);
+    public Task<UserDto?> SelectUserBySpotifyIdAsync(string userId);
+    public Task<PlaylistDto?> InsertPlaylistAsync(PlaylistDto playlist);
+}
