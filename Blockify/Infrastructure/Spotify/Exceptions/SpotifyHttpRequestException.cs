5c76d1b366ab562c13fcf72dc3e178ec137155b3
DanielPadre58 <2kupwhite@gmail.com>
2025-09-19 00:54:15 +0100

    feat: Exception that servess as base for spotify related exceptions when it comes from spotify api requests

---
+++ Blockify/Infrastructure/Spotify/Exceptions/SpotifyHttpRequestException.cs
@@ -0,0 +1,23 @@
+namespace Blockify.Infrastructure.Spotify.Exceptions;
+
+public class SpotifyHttpRequestException : Exception
+{
+    public string EndpointUri { get; }
+    public string SpotifyMessage { get; }
+
+    public SpotifyHttpRequestException(string message)
+        : this(string.Empty, message, string.Empty, null) { }
+
+    public SpotifyHttpRequestException(string? uri, string message)
+        : this(uri, message, string.Empty, null) { }
+
+    public SpotifyHttpRequestException(string? uri, string message, string spotifyMessage)
+        : this(uri, message, spotifyMessage, null) { }
+
+    public SpotifyHttpRequestException(string? uri, string message, string spotifyMessage, Exception? innerException)
+        : base(message, innerException)
+    {
+        EndpointUri = uri ?? string.Empty;
+        SpotifyMessage = spotifyMessage;
+    }
+}
