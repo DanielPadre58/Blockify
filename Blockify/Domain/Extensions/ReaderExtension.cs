57db44008e0dfdb50fa761f8aab73f844baa96d2
DanielPadre58 <2kupwhite@gmail.com>
2025-09-18 01:24:05 +0100

    feat: Extension to the NpgsqlDataReader class to parse data rows to a dto, currently for User and Playlist

---
+++ Blockify/Domain/Extensions/ReaderExtension.cs
@@ -0,0 +1,43 @@
+using Blockify.Application.DTOs;
+using Blockify.Application.DTOs.Authentication;
+using Npgsql;
+
+namespace Blockify.Domain.Extensions;
+
+public static class ReaderExtension
+{
+    public async static Task<UserDto> ReadUserAsync(this NpgsqlDataReader data)
+    {
+        await data.ReadAsync();
+
+        return new UserDto
+        {
+            Id = Convert.ToInt64(data["id"]),
+            Email = data["email"].ToString() ?? string.Empty,
+            Spotify = new SpotifyDto
+            {
+                Id = data["spotify_id"].ToString() ?? string.Empty,
+                Url = data["spotify_profile_url"].ToString() ?? string.Empty,
+                Username = data["spotify_username"].ToString() ?? string.Empty,
+                Token = new TokenDto
+                {
+                    RefreshToken = data["spotify_refresh_token"].ToString() ?? string.Empty,
+                    AccessToken = data["spotify_access_token"].ToString() ?? string.Empty,
+                    ExpiresAt = Convert.ToDateTime(data["spotify_expires_at"])
+                }
+            }
+        };
+
+    }
+    
+    public async static Task<PlaylistDto> ReadPlaylistAsync(this NpgsqlDataReader data)
+    {
+        await data.ReadAsync();
+
+        return new PlaylistDto
+        {
+            OwnerId = data["owner_id"].ToString() ?? string.Empty,
+            SpotifyId = data["spotify_id"].ToString() ?? string.Empty,
+        };
+    }
+}
