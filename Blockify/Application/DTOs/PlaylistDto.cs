a082dce57bd2351ba577b5b23b3d251cfe4dd05e
DanielPadre58 <2kupwhite@gmail.com>
2025-09-18 01:22:40 +0100

    refac: Name field on playlist dto is empry by default

--- Blockify/Application/DTOs/PlaylistDto.cs
+++ Blockify/Application/DTOs/PlaylistDto.cs
@@ -5,6 +5,6 @@ public record PlaylistDto
 {
     public required string OwnerId { get; set; }
     public required string SpotifyId { get; set; }
-    public required string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
 }
