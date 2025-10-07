6d6fd5bf6f3cb2cf79cfa38d1ee5a2862f8d2772
DanielPadre58 <2kupwhite@gmail.com>
2025-10-07 22:13:23 +0100

    Added a method that reads a builds a token dto based on response from database query, and exception handling aswell

--- Blockify/Domain/Extensions/ReaderExtension.cs
+++ Blockify/Domain/Extensions/ReaderExtension.cs
@@ -1,35 +1,65 @@
+using System.Security.Cryptography;
 using Blockify.Application.DTOs;
 using Blockify.Application.DTOs.Authentication;
+using Blockify.Infrastructure.Exceptions.Blockify;
 using Npgsql;
 
 namespace Blockify.Domain.Extensions;
 
 public static class ReaderExtension
 {
-    public async static Task<UserDto> ReadUserAsync(this NpgsqlDataReader data)
+    public async static Task<UserDto?> ReadUserAsync(this NpgsqlDataReader data)
+    {
+        try
+        {
+            await data.ReadAsync();
+
+            if (!data.HasRows)
+                return null;
+
+            var user = new UserDto
+            {
+                Id = Convert.ToInt64(data["id"]),
+                Email = data["email"].ToString() ?? string.Empty,
+                Spotify = new SpotifyDto
+                {
+                    Id = data["spotify_id"].ToString() ?? string.Empty,
+                    Url = data["spotify_profile_url"].ToString() ?? string.Empty,
+                    Username = data["spotify_username"].ToString() ?? string.Empty,
+                    Token = new TokenDto
+                    {
+                        RefreshToken = data["spotify_refresh_token"].ToString() ?? string.Empty,
+                        AccessToken = data["spotify_access_token"].ToString() ?? string.Empty,
+                        ExpiresAt = Convert.ToDateTime(data["spotify_expires_at"]),
+                        ExpiresIn = Convert.ToInt32(
+                                (Convert.ToDateTime(data["spotify_expires_at"]) - DateTime.Now).TotalSeconds)
+                    }
+                }
+            };
+
+            return user;
+        }
+        catch (InvalidCastException ex)
+        {
+            throw new InvalidColumnOperationException(ex.Message);
+        }
+    }
+
+    public async static Task<TokenDto?> ReadTokenAsync(this NpgsqlDataReader data)
     {
         await data.ReadAsync();
 
-        return new UserDto
-        {
-            Id = Convert.ToInt64(data["id"]),
-            Email = data["email"].ToString() ?? string.Empty,
-            Spotify = new SpotifyDto
-            {
-                Id = data["spotify_id"].ToString() ?? string.Empty,
-                Url = data["spotify_profile_url"].ToString() ?? string.Empty,
-                Username = data["spotify_username"].ToString() ?? string.Empty,
-                Token = new TokenDto
-                {
-                    RefreshToken = data["spotify_refresh_token"].ToString() ?? string.Empty,
-                    AccessToken = data["spotify_access_token"].ToString() ?? string.Empty,
-                    ExpiresAt = Convert.ToDateTime(data["spotify_expires_at"]),
-                    ExpiresIn = Convert.ToInt32(
-                        (Convert.ToDateTime(data["spotify_expires_at"]) - DateTime.Now).TotalSeconds)
-                }
-            }
-        };
+        if (!data.HasRows)
+            return null!;
 
+        return new TokenDto
+        {
+            AccessToken = data["spotify_access_token"].ToString()!,
+            ExpiresAt = Convert.ToDateTime(data["spotify_expires_at"]),
+            ExpiresIn = Convert.ToInt32(
+                (Convert.ToDateTime(data["spotify_expires_at"]) - DateTime.Now).TotalSeconds),
+            RefreshToken = data["spotify_refresh_token"].ToString()!
+        };
     }
     
     public async static Task<PlaylistDto> ReadPlaylistAsync(this NpgsqlDataReader data)
