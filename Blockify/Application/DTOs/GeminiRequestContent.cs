c6d89beb68dda88d34fa8a69c84d6a672f607d75
DanielPadre58 <2kupwhite@gmail.com>
2025-10-08 23:05:38 +0100

    Dto that will store all information that the AI will need to determine the song keyword as well as some additional (optional) contextual information

--- Blockify/Application/DTOs/GeminiRequestContent.cs
+++ Blockify/Application/DTOs/GeminiRequestContent.cs
@@ -1,7 +1,9 @@
 ﻿namespace Blockify.Application.DTOs;
 
-public record GeminiSongData
+public record GeminiRequestContent
 {
+    public required string Objective { get; init; }
+    public List<string>? ExistingKeywords { get; init; }
     public required string Name { get; init; }
     public string? Artist { get; init; }
     public string? Genre { get; init; }
