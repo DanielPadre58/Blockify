53f484805594c191287562f95272e070fabe2541
DanielPadre58 <2kupwhite@gmail.com>
2025-10-08 22:55:30 +0100

    Name field is now required, because it is needed to identify all userss keywords

--- Blockify/Application/DTOs/PlaylistDto.cs
+++ Blockify/Application/DTOs/PlaylistDto.cs
@@ -5,6 +5,6 @@ public record PlaylistDto
 {
     public required string OwnerId { get; init; }
     public required string SpotifyId { get; init; }
-    public string? Name { get; init; }
+    public required string Name { get; init; }
     public string? Description { get; init; }
 }
