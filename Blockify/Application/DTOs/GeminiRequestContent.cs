6e672166586a2d107715f3876dbb2eda5e55c6a6
DanielPadre58 <2kupwhite@gmail.com>
2025-10-08 12:46:57 +0100

    Dto that will hold the information about the song that gemini will analyze

---
+++ Blockify/Application/DTOs/GeminiRequestContent.cs
@@ -0,0 +1,9 @@
+﻿namespace Blockify.Application.DTOs;
+
+public record GeminiSongData
+{
+    public required string Name { get; init; }
+    public string? Artist { get; init; }
+    public string? Genre { get; init; }
+    public required string Lyrics { get; init; }
+}
