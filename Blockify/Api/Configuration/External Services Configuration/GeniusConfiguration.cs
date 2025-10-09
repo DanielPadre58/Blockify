1454934648365cc02126d92337709fbba2a13a59
DanielPadre58 <2kupwhite@gmail.com>
2025-10-09 09:55:30 +0100

    Class responsible for storing Genius configurations data

---
+++ Blockify/Api/Configuration/External Services Configuration/GeniusConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Blockify.Api.Configuration;
+
+public class GeniusConfiguration
+{
+    public required string ClientId { get; set; }
+    public required string ClientSecret { get; set; }
+}
