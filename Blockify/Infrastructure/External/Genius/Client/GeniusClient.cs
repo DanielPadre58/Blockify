277b13a5edb38c61ce02224da8a0bc8c760bdce4
DanielPadre58 <2kupwhite@gmail.com>
2025-10-10 11:52:52 +0100

    Search for Genius song Id by song name and anrtist

---
+++ Blockify/Infrastructure/External/Genius/Client/GeniusClient.cs
@@ -0,0 +1,26 @@
+﻿using Blockify.Infrastructure.External.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Blockify.Infrastructure.External.Genius.Client;
+
+public class GeniusClient : ExternalClient, IGeniusClient
+{
+    private readonly GeniusConfiguration _configuration;
+
+    public GeniusClient(HttpClient client, IOptions<GeniusConfiguration> configuration) : base(client)
+    {
+        _configuration = configuration.Value;
+    }
+
+    public async Task<HttpResponseMessage> SearchSongId(string name, string artist)
+    {
+        var requestUri = $"https://api.genius.com/search?q={Uri.EscapeDataString(name)}%20{Uri.EscapeDataString(artist)}";
+        var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+        request.Headers.Add("Authorization", $"Bearer {_configuration.ClientSecret}");
+
+        var response = await HttpClient.SendAsync(request);
+        await VerifyResponseAsync(response, request);
+
+        return response;
+    }
+}
