73acef4c7baab476c76924f1113632a5725d29e8
DanielPadre58 <2kupwhite@gmail.com>
2025-10-11 01:01:08 +0100

    Add SongDto record for song data representation

---
+++ Blockify/Application/DTOs/SongDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Blockify.Application.DTOs;
+
+public record SongDto
+{
+    public string Name { get; init; }
+    public string Artist { get; init; }
+    public string Genre { get; init; }
+}
